4be8b50b4edcc6e621267506a72147d5f7b17d19
Plantere <bfabiobenfica@gmail.com>
2023-12-04 11:10:35 -0300

    Refactor: Putting all function related with modbus in class

--- modBusComunication.py
+++ modBusComunication.py
@@ -1,60 +1,31 @@
 #!/usr/bin/env python
 import minimalmodbus
-#
-import numpy as np
-import serial.tools.list_ports
 
-
-# instrument.serial.port                     # this is the serial port name
-# instrument.serial.bytesize = 8
-# instrument.serial.parity   = serial.PARITY_NONE
-# instrument.serial.stopbits = 1
-# instrument.serial.timeout  = 0.05          # seconds
-
-def listar_portas_seriais():
-    # Lista as portas seriais disponíveis no PC atual
-    portas = list(serial.tools.list_ports.comports())
-    # Cria uma lista com os nomes das portas seriais
-    nomes_portas = [porta.device for porta in portas]
-    return nomes_portas
-
-
-def read_Input_Registers(instrument):
-    # EndereçoDeMemória -> 00 C0 -> 192
-    leituraDados = instrument.read_register(registeraddress=192)
-    print(f"registros de entrada(decimal): {leituraDados}")
-    input_reg = decimal_para_vetor(leituraDados)
-    print(f"registros de entrada(binario): {input_reg}")
-    return input_reg
-
-
-def write_Output_Registers(output_reg, instrument):
-    # Conversão do vetor(binario) para decimal(valor)
-    valorWrite = vetor_para_decimal(output_reg)
-    # EndereçoDeMemória -> 00 70 -> 112
-    instrument.write_register(registeraddress=112, value=valorWrite)
-
-
-def vetor_para_decimal(vetor):
-    # Converte o vetor em uma string binária
-    binario = ''.join([str(bit) for bit in vetor])
-    # Converte a string binária em um número decimal
-    decimal = int(binario, 2)
+def array_to_decimal(array):
+    binary = ''.join([str(bit) for bit in array])
+    decimal = int(binary, 2)
     return decimal
 
+def decimal_to_array(decimal):
+    binary = '{0:08b}'.format(decimal)
+    array = [int(bit) for bit in binary]
+    return array
 
-def decimal_para_vetor(decimal):
-    # Converte o número decimal em uma string binária de 8 bits
-    binario = '{0:08b}'.format(decimal)
-    # Converte a string binária em um vetor de inteiros
-    vetor = [int(bit) for bit in binario]
-    return vetor
+class Modbus:
+    def __init__(self):
+        self.instrument = False;
+        self.input_reg = False;
+    
+    def get_instrument(self, port):
+        self.instrument = minimalmodbus.Instrument(port, 1)
+        self.instrument.mode = minimalmodbus.MODE_RTU
+        self.instrument.serial.baudrate = 9600
+        self.instrument.clear_buffers_before_each_transaction = True
 
+    def read_input_registers(self):
+        self.input_reg = decimal_to_array(self.instrument.read_register(192))
+        return self.input_reg
 
-def obterInstrumento(portaSerialSelecionada):
-    # port name, slave address (in decimal)
-    instrument = minimalmodbus.Instrument(portaSerialSelecionada, 1)
-    instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
-    instrument.serial.baudrate = 9600   # Baud
-    instrument.clear_buffers_before_each_transaction = True
-    return instrument
+    def write_output_registers(self, output_reg):
+        self.instrument.write_register(112, array_to_decimal(output_reg))
+
