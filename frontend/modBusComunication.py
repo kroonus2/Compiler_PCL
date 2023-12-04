1474c6e63da83e149906002f3d57b345bcb98695
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2023-12-04 16:18:48 -0300

    Update modBusComunication.py

--- frontend/modBusComunication.py
+++ frontend/modBusComunication.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 import minimalmodbus
 
+
 def array_to_decimal(array):
     binary = ''.join([str(bit) for bit in array])
     decimal = int(binary, 2)
     return decimal
+
 
 def decimal_to_array(decimal):
     binary = '{0:08b}'.format(decimal)
     array = [int(bit) for bit in binary]
     return array
 
+
 class Modbus:
     def __init__(self):
-        self.instrument = False;
-        self.input_reg = False;
-    
+        self.instrument = False
+        self.input_reg = [0, 0, 0, 0, 0, 0, 0, 0]
+
     def get_instrument(self, port):
         self.instrument = minimalmodbus.Instrument(port, 1)
         self.instrument.mode = minimalmodbus.MODE_RTU
@@ -24,8 +27,9 @@ class Modbus:
 
     def read_input_registers(self):
         self.input_reg = decimal_to_array(self.instrument.read_register(192))
+        print(self.input_reg)
+        print(self.instrument.read_register(192))
         return self.input_reg
 
     def write_output_registers(self, output_reg):
         self.instrument.write_register(112, array_to_decimal(output_reg))
-
