0eb7e9237074c7d6f5aff39d08a8e0cb193d5dd1
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2023-12-09 14:13:18 -0300

    Create modbusConfigManager.py

---
+++ frontend/modbusConfigManager.py
@@ -0,0 +1,28 @@
+import json
+
+
+class ModbusConfigManager:
+    def __init__(self):
+        self.config_file = 'backend/src/main/java/iftm/compilador/clp/storage/communication.json'
+
+    def carregar_configuracoes(self):
+        try:
+            with open(self.config_file, 'r') as arquivo_config:
+                configuracoes = json.load(arquivo_config)
+                return configuracoes
+        except FileNotFoundError:
+            return None
+
+    def salvar_configuracoes(self, configuracoes):
+        with open(self.config_file, 'w') as arquivo_config:
+            json.dump(configuracoes, arquivo_config, indent=4)
+
+    def atualizar_input_reg(self, novo_input_reg):
+        configuracoes = self.carregar_configuracoes()
+        if configuracoes is not None:
+            input_reg_atual = configuracoes.get('INPUT', [])
+            if input_reg_atual != novo_input_reg:
+                configuracoes['INPUT'] = novo_input_reg
+                self.salvar_configuracoes(configuracoes)
+            else:
+                print("NÃO HOUVE ALTERAÇÕES NAS ENTRADAS")
